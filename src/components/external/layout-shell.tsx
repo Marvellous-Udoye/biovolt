dc67ac1b1a3024893cfac08f30fbff419875fb64
unknown <marveldoc17@gmail.com>
2026-06-08 13:32:53 +0100

    feat: add scroll-triggered reveal animations

--- src/components/external/layout-shell.tsx
+++ src/components/external/layout-shell.tsx
@@ -1,7 +1,12 @@
+"use client";
+
+import { useScrollReveal } from "@/lib/use-scroll-reveal";
 import { Footer } from "./footer";
 import { Navbar } from "./navbar";
 
 export function ExternalShell({ children }: { children: React.ReactNode }) {
+  useScrollReveal();
+
   return (
     <div className="min-h-screen bg-[#fbfcf7] text-[#242824]">
       <Navbar />
